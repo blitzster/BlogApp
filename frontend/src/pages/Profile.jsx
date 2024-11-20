17740b47fba2615d0a9df8ffcb27b3bdf1149f12
Akshay S <blitz122333@gmail.com>
2024-11-20 22:45:43 +0530

    added profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,23 +1,39 @@
 import { BlogCard } from "../components/BlogCard"
 import { useState, useEffect } from "react"
-import { getAllPosts } from '../api'
+import { getPosts } from '../api'
+import * as jwt_decode from 'jwt-decode'
 
 
 export function Profile() {
 
-    const [post, setPosts] = useState([])
+    const [posts, setPosts] = useState([])
     const [user, setUser] = useState({})
 
     useEffect(() => {
         async function loadUserData() {
-
+            const token = sessionStorage.getItem("User")
+            const decodedUser = jwt_decode.jwtDecode(token)
+            const allPosts = await getPosts()
+            const filteredPosts = allPosts.filter((post) => post.author == decodedUser._id)
+            setPosts(filteredPosts)
+            setUser(decodedUser)
         }
         loadUserData()
     }, [])
 
     return (
         <>
-            Profile
+            <label >Name: </label>
+            <h2>{user.name}</h2>
+            <label >Email: </label>
+            <h2>{user.email}</h2>
+            <label >Join Date: </label>
+            <h2>{user.joinDate}</h2>
+            {posts.map((post) => {
+                return (
+                    <BlogCard post={post} />
+                )
+            })}
         </>
     )
 }
